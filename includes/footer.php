22a9b92165c0568017ab7be07cf142a2e38b383d
Abdul Moiz <moizrauf03@gmail.com>
2021-05-25 05:43:41 +0500

    d

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
       <div class="pull-right hidden-xs">
         <b>All rights reserved</b>
       </div>
-      <strong>Copyright &copy; 2021BroughtC to You By>Abdul moiz</a></strong>
+      <strong>Copyright &copy; 2021BroughtdC to You By>Abdul moiz</a></strong>
     </div>
 </footer>
